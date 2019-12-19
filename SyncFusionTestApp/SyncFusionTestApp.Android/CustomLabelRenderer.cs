8e3f83de52a30f4422f304a32406cea987d30b41
norton287 <42098364+norton287@users.noreply.github.com>
2019-12-19 07:35:35 -0600

    Update layout

---
+++ SyncFusionTestApp/SyncFusionTestApp.Android/CustomLabelRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content;
+using Android.Runtime;
+
+using SyncFusionTestApp.Droid;
+
+using System;
+using Xamarin.Forms;
+
+#pragma warning disable 612
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+#pragma warning restore 612
+namespace SyncFusionTestApp.Droid
+{
+    [Obsolete]
+    public class CustomLabelRenderer : Xamarin.Forms.Platform.Android.FastRenderers.LabelRenderer
+    {
+        public CustomLabelRenderer(Context context) : base(context)
+        {
+
+        }
+
+        [Obsolete]
+        public CustomLabelRenderer(IntPtr handle, JniHandleOwnership transfer)
+        {
+
+        }
+    }
+}
